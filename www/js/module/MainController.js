a3af342f53b7ffbcba1c7608becb3e50744b676d
sepei <use3@bastronico.at>
2014-03-13 10:03:52 +0100

    abc
    abc

--- www/js/module/MainController.js
+++ www/js/module/MainController.js
@@ -1,14 +1,28 @@
 var mCon = angular.module('MainController', []);
 
-mCon.controller ( 'MainAppCtrl', function ( $scope ) {
+mCon.controller ( 'MainAppCtrl', function ( $scope , $http ) {
 	//alert("Controller Alert");
 	
 	console.log($scope);
 	
 	$scope.status = "Nicht verbunden";
 	
+	$scope.doIT = function (a) {
+		//alert(a);
+		toggleO('a'+a)
+	};
+	
 	$scope.onClick = function () {
 		$scope.status = "verbunden";
         $scope.setOverlay = true;
+		
+		$http({
+  			method: 'JSONP',
+  			url: 'http://23.249.160.7:8080/Serveranwendung/index.jsp?anz=' + $("#search").val() + '&callback=JSON_CALLBACK'
+			}).success(function(data, status, headers, config) {
+				$scope.medi = data;
+			}).error(function(data, status, headers, config) {
+			});
+		
 	}
 });
